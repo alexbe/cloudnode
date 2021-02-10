d095acab4f6bcc820ee4d8d9a96be8ab2e36faa7
Alexander Bechkov <bechkov@gmail.com>
2021-02-10 20:04:14 +0300

    fix sync.sh

--- sync.sh
+++ sync.sh
@@ -6,12 +6,15 @@ INSTDIR=`realpath ${1:-~/salt-ssh}`
 
 install2()
 {
-
-[ -d "${INSTDIR}" || mkdir "${INSTDIR}"
-[ -f "${INSTDIR}/Saltfile" ] && [ -z "$(grep '^${INSTDIR}' $CFGDIR/installs)" ] \
+[ -d "$CFGDIR" ] || ( mkdir "$CFGDIR" && echo "Cfg diectory created" )
+[ -d "${INSTDIR}" ] || ( mkdir "${INSTDIR}" && echo "Directory '${INSTDIR}' created" )
+[ -f "${INSTDIR}/Saltfile" -a -z "$(grep ^${INSTDIR} $CFGDIR/installs)" ] \
   && echo "Found unknown installation '${INSTDIR}'" && exit 1
 
-cat <<THECONTENT > ${CFGDIR}/excl.list
+cat <<THECONTENT > ${CFGDIR}/excl.list && rsync -av --exclude=.git/ --exclude=.gitignore  ${CD}/ ${INSTDIR}/
+.git/
+.gitignore
+local/
 roster
 Saltfile
 master
@@ -21,18 +24,17 @@ cloud.providers.d/
 pki/
 nodemap
 THECONTENT
-
-echo ${INSTDIR} >> ${CFGDIR}/installs
-
+[ -z "$(grep ^${INSTDIR} $CFGDIR/installs)" ] && echo ${INSTDIR} >> ${CFGDIR}/installs
 }
 
 
-
-if [ -d "$CFGDIR" -a -f "${CFGDIR}/excl.list" ]; then
+if [ -d "$CFGDIR" -a -d "${INSTDIR}" -a -n "$(grep ^${INSTDIR} $CFGDIR/installs)" ]; then
  while read IPATH
  do
   [ -d "$IPATH" ] && rsync -av --exclude-from="${CFGDIR}/excl.list" $CD/ $IPATH/
- done < ${CFGDIR}/excl.list
+ done < ${CFGDIR}/installs
+ echo "...syncronized"\!
 else
+ echo "Perform new install"
  install2
 fi
